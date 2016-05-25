7209d76d818f0f3a738cc037f2004c44b8540820
Stefan Marr <git@stefan-marr.de>
2016-05-25 11:44:40 +0200

    Add FailOnMissingOptimizations flag in Universe, like in SOMns’ VM
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vm/Universe.java
+++ src/som/vm/Universe.java
@@ -43,6 +43,14 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
+import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.TruffleRuntime;
+import com.oracle.truffle.api.frame.MaterializedFrame;
+import com.oracle.truffle.api.object.DynamicObject;
+
 import som.compiler.Disassembler;
 import som.interpreter.Invokable;
 import som.interpreter.TruffleCompiler;
@@ -57,15 +65,15 @@ import som.vmobjects.SInvokable.SPrimitive;
 import som.vmobjects.SObject;
 import som.vmobjects.SSymbol;
 
-import com.oracle.truffle.api.CompilerAsserts;
-import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
-import com.oracle.truffle.api.Truffle;
-import com.oracle.truffle.api.TruffleRuntime;
-import com.oracle.truffle.api.frame.MaterializedFrame;
-import com.oracle.truffle.api.object.DynamicObject;
-
 public final class Universe {
+
+  public static final boolean FailOnMissingOptimizations = false;
+
+  public static void callerNeedsToBeOptimized(final String msg) {
+    if (FailOnMissingOptimizations) {
+      CompilerAsserts.neverPartOfCompilation(msg);
+    }
+  }
 
   /**
    * Associations are handles for globals with a fixed
