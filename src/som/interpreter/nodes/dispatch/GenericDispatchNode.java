2617a83abd277fc7ed974c69849ebe165f1197a0
Stefan Marr <git@stefan-marr.de>
2016-05-25 11:45:43 +0200

    Make case of polymorphic method dispatch compilable
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/dispatch/GenericDispatchNode.java
+++ src/som/interpreter/nodes/dispatch/GenericDispatchNode.java
@@ -1,32 +1,38 @@
 package som.interpreter.nodes.dispatch;
 
-import som.interpreter.SArguments;
-import som.interpreter.Types;
-import som.vmobjects.SArray;
-import som.vmobjects.SClass;
-import som.vmobjects.SInvokable;
-import som.vmobjects.SSymbol;
-
 import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.IndirectCallNode;
 import com.oracle.truffle.api.object.DynamicObject;
 
+import som.interpreter.SArguments;
+import som.primitives.ObjectPrims.ClassPrim;
+import som.primitives.ObjectPrimsFactory.ClassPrimFactory;
+import som.vm.Universe;
+import som.vmobjects.SArray;
+import som.vmobjects.SClass;
+import som.vmobjects.SInvokable;
+import som.vmobjects.SSymbol;
+
 public final class GenericDispatchNode extends AbstractDispatchNode {
   @Child private IndirectCallNode call;
+  @Child private ClassPrim getClass;
   protected final SSymbol selector;
 
   public GenericDispatchNode(final SSymbol selector) {
     this.selector = selector;
     call = Truffle.getRuntime().createIndirectCallNode();
+    getClass = ClassPrimFactory.create(null);
   }
 
   @Override
   public Object executeDispatch(
       final VirtualFrame frame, final Object[] arguments) {
+    Universe.callerNeedsToBeOptimized("We should not reach this, ideally, in benchmark code");
+
     Object rcvr = arguments[0];
-    DynamicObject rcvrClass = Types.getClassOf(rcvr);
+    DynamicObject rcvrClass = (DynamicObject) getClass.executeEvaluated(null, rcvr);
     SInvokable method = SClass.lookupInvokable(rcvrClass, selector);
 
     CallTarget target;
